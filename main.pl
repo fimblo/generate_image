02431996e6f0621cf16ce000175d2c9cd81dd1b0
Mattias Jansson <fimblo@yanson.org>
2020-02-07 17:43:16 +0100

    cli support for ratios. also retries.
    If no real change for two generations, it just randomizes
    the best gene.

--- main.pl
+++ main.pl
@@ -33,25 +33,28 @@ use Genes qw/
 # Help message
 my $basename = basename($0);
 my $helptext = << "EOM";
-Generate an image which, over generations, approximates target image.
+  Generate an image which, over generations, approximates target image.
 
-Usage: $basename -t <target-file> [-h -s <seed> -i <iter> -p <pool>]
+ Usage: $basename -t <target-file> [-h -s <seed> -i <iter> -p <pool>]
 
-       -t <target-file>  # image to approximate
+  -t <target-file>  # image to approximate
 
-Optional params
-       -s <seed>         # start first iteration with this seed file.
-       -i <iter>         # number of iterations. (default 10)
-       -p <pool>         # size of gene pool. (default 10)
-       -h                # This help message
+  Optional params
+  -s <seed>         # start first iteration with this seed file.
+  -i <iter>         # number of iterations. (default 10)
+  -r <ratio>        # population ratio for next generation.
+  # Survivor:Children:Mutants (default 2:4:4)
+  -p <pool>         # size of gene pool. (default 10)
+  -h                # This help message
 EOM
 
-# --------------------------------------------------
-# Go through commandline options
+  # --------------------------------------------------
+  # Go through commandline options
 my $target_image_filename = undef;
 my $seed_file = undef;
 my $iterations = 10;
 my $pool = 10;
+my $ratio = "2:4:4";
 my $help;
 
 GetOptions(
@@ -59,6 +62,7 @@ GetOptions(
   "seed=s"       => \$seed_file,
   "iterations=i" => \$iterations,
   "pool=i"       => \$pool,
+  "ratio=s"      => \$ratio,
   "help"         => \$help,
   ) or die ("bad commandline args\n");
 
@@ -68,14 +72,32 @@ if (! $target_image_filename or $help ) {
 }
 # --------------------------------------------------
 
-# Create an array of genes
 &set_max_population($pool);
-&set_survival_percent(0.2);
-&set_mate_percent(0.4);
-&set_mutate_percent(0.4);
+
+unless ($ratio =~ m/^(\d+):(\d+):(\d+)$/) {
+  print << "EOM";
+  Ratio should be specified as three integers separated by colons.
+
+  For example, in the command below the -r param tells the program that
+  for every two survivors, you want four children and five mutants in
+  the next generation.
+
+  $basename -t $target_image_filename -r 2:4:5
+EOM
+  exit 1;
+}
+my ($s,$c,$m) = ($1, $2, $3); # capture digits from regex above
+my $tot = $s+$c+$m;
+&set_survival_percent($s/$tot);
+&set_mate_percent($c/$tot);
+&set_mutate_percent($m/$tot);
+
+
+# Create an array of genes
 my $population = &generate_genes($seed_file); # if undef, starts from scratch.
 
 my $prev_best_distance = 1;
+my $prev_distance_diff = 1;
 for (my $i = 0; $i < $iterations; $i++) {
 
   # Create images from population
@@ -89,24 +111,35 @@ for (my $i = 0; $i < $iterations; $i++) {
   my @best_genes = @{$population}[@$best_indices];
   my $mutants = &mutate_population(\@best_genes);
   my $children = &mate_population(\@best_genes);
-  $population = [ @best_genes, @$children, @$mutants];
 
 
 
 
   # --------------------------------------------------
   # Output status for user
+  my $best_distance = &set_best_distance();
+  my $distance_diff = $prev_best_distance - $best_distance;
   my $b_pop = scalar @best_genes;
   my $m_pop = scalar @$mutants;
   my $c_pop = scalar @$children;
-  my $best_distance = &set_best_distance();
-  my $distance_diff = $prev_best_distance - $best_distance;
-
   my ($max, $avg, $stdev) = &get_gene_len_stats(\@best_genes);
-
   print "Round $i: (S:$b_pop C:$c_pop M:$m_pop) (Gene length Max:$max Avg:$avg StdDev:$stdev)\n";
   print "Best distance: $best_distance\t(diff: $distance_diff)\n";
+
+
+  # --------------------------------------------------
+  # If we have no real progress for two rounds, then repopulate
+  # population using the best 5 genes, all mutants
+  if (($prev_distance_diff + $distance_diff) < 0.0001) {
+    print "There was no real change for two cycles. Shaking things up a bit.\n";
+    my $prev = $i-1;
+    $population = &generate_genes("$$/gene_$prev.txt");
+  }
+  else {
+    $population = [ @best_genes, @$children, @$mutants];
+  }
   $prev_best_distance = $best_distance;
+  $prev_distance_diff = $distance_diff;
 
 
   # --------------------------------------------------
